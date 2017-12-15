9ca732d123b6f4aa1218f66430d207fa05182bdd
Ubuntu <Ajitesh>
2017-12-15 03:36:42 -0800

    Placed {noop} in front of password

--- healthapp-spring/src/main/resources/data.sql
+++ healthapp-spring/src/main/resources/data.sql
@@ -3,5 +3,5 @@ INSERT INTO oauth_client_details
     web_server_redirect_uri, authorities, access_token_validity,
     refresh_token_validity, additional_information, autoapprove)
 VALUES
-    ("healthapp", "HeAltH@!23", "read,write,doctor",
+    ("healthapp", "{noop}HeAltH@!23", "read,write,doctor",
     "password,authorization_code,refresh_token", null, null, 3600, 36000, null, true);
